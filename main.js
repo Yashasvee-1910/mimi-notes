1bb384ce64e7cee3af0c08c393347dfdd54c0f67
Yashasvee <mehrotrayashasvee@gmail.com>
2025-03-20 20:06:11 +0530

    v1.0.0

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-const { app, BrowserWindow } = require('electron'); // ✅ You missed this line!
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
 function createWindow() {
+  const iconPath = process.platform === 'darwin' 
+    ? path.join(__dirname, 'assets/icons/mimi.icns')
+    : path.join(__dirname, 'assets/icons/mimi.png'); // Use .ico for Windows
+
   const mainWindow = new BrowserWindow({
-    width: 600,
-    height: 628,
+    width: 450,
+    height: 478,
+    icon: iconPath,
     resizable: false,
     webPreferences: {
       nodeIntegration: true,
@@ -16,8 +22,16 @@ function createWindow() {
 
 app.whenReady().then(createWindow);
 
+// Quit when all windows are closed, except on macOS
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
+  }
+});
+
+app.on('activate', () => {
+  // On macOS, re-create a window when the dock icon is clicked and there are no other windows open
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
   }
 });
